5ee0e9bb17631047fac4a7866f1c15f2539f38c3
Alejandra García <alejandra.patzan@galileo.edu>
2022-03-02 16:49:44 -0600

    prueba 4

--- threads/thread.h
+++ threads/thread.h
@@ -90,6 +90,8 @@ struct thread
     int priority;                       /* Priority. */
     struct list_elem allelem;           /* List element for all threads list. */
 
+  	int tim_sleep;
+
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
 
