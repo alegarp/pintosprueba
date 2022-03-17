2e9b6495d4c81ac27365f05472b8e92f831a53f2
Alejandra García <alejandra.patzan@galileo.edu>
2022-03-16 23:34:46 -0600

    ale

--- threads/thread.h
+++ threads/thread.h
@@ -140,4 +140,11 @@ void thread_set_nice (int);
 int thread_get_recent_cpu (void);
 int thread_get_load_avg (void);
 
+
+
+
+
+
+void remover_thread_durmiente(int64_t ticks);
+void insertar_en_lista_espera(int64_t ticks);
 #endif /* threads/thread.h */
