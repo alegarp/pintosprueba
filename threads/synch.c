5a4e0a80b484587efc7d6792f9a1fba1b7568b86
Diegomjx <19002145@galileo.edu>
2022-04-23 09:29:13 -0600

    V0.0.23.2 es list_front no list_pop_front

--- threads/synch.c
+++ threads/synch.c
@@ -140,7 +140,7 @@ sema_up (struct semaphore *sema)
   old_level = intr_disable ();
   if (!list_empty (&sema->waiters)){
         list_sort(&sema->waiters,&ordered_thread,aux);
-        int thread_priority = list_entry (list_pop_front (&sema->waiters),
+        int thread_priority = list_entry (list_front (&sema->waiters),
                                 struct thread, elem)->priority;
         thread_unblock (list_entry (list_pop_front (&sema->waiters),
                                 struct thread, elem));
