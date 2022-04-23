6c64a824eea04521b8af22f6b18cdcbad461ea72
Diegomjx <19002145@galileo.edu>
2022-04-23 00:04:46 -0600

    V0.0.21 falto iniciar listas jajaja

--- threads/thread.c
+++ threads/thread.c
@@ -608,7 +608,9 @@ init_thread (struct thread *t, const char *name, int priority)
   }
   t->dono = false;
   t->magic = THREAD_MAGIC;
-
+  list_init(&t->Locks);
+  list_init(&t->locksTryAcquire);
+  t->locks_intentan_adquirir=NULL;
 
   old_level = intr_disable ();
   list_push_back (&all_list, &t->allelem);
