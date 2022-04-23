247fc9bea156349f3e8333ba2f632f00a346dbe7
Diegomjx <19002145@galileo.edu>
2022-04-23 16:04:41 -0600

    V0.0.24.10.3

--- threads/synch.c
+++ threads/synch.c
@@ -431,7 +431,7 @@ cond_signal (struct condition *cond, struct lock *lock UNUSED)
 
   if (!list_empty (&cond->waiters)) {
         //deveria ordenarlo??
-    //    list_sort(&cond->waiters,&ordered_cond ,aux);
+        list_sort(&cond->waiters,&ordered_cond ,aux);
         sema_up (&list_entry (list_pop_front (&cond->waiters),
                           struct semaphore_elem, elem)->semaphore);
 
